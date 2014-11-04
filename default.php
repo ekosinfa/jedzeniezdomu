02d04a787b47db95a38218d484956a345b82ac5e
michubest <michubest@gmail.com>
2014-11-04 21:19:30 +0100

    Update default.php

--- default.php
+++ default.php
@@ -1,4 +1,30 @@
 <?php
 echo '<h3><font  face="Verdana">Zamawiaj obiad przez internet!</font><h3>';
 
+$con=mysqli_connect("localhost","michal","michalxsw21qaz","ekosinfa_michal");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM menu WHERE type ='pizza'");
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['FirstName'] . "</td>";
+echo "<td>" . $row['LastName'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
 ?>
