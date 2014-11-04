2e6bd9ab52bf1b44aac4406fd0190f8025e3e2eb
michubest <michubest@gmail.com>
2014-11-04 21:24:15 +0100

    Update default.php

--- default.php
+++ default.php
@@ -14,13 +14,15 @@ echo "<table border='1'>
 <tr>
 <th>nazwa</th>
 <th>cena</th>
+<th>opis</th>
 </tr>";
 
 while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
 echo "<td>" . $row['name'] . "</td>";
-echo "<td>" . $row['price'] . "</td>";
+echo "<td>" . $row['description'] . "</td>";
+echo "<td>" . $row['cost'] . "</td>";
 echo "</tr>";
 }
 echo "</table>";
