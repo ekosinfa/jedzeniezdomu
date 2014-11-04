8a6d1e47f7f5cabd2dfb50e836ccbd97ee403172
michubest <michubest@gmail.com>
2014-11-04 21:15:14 +0100

    Update default.php
    Usunąłem wszystko D:

--- default.php
+++ default.php
@@ -1,26 +1,4 @@
 <?php
 echo '<h3><font  face="Verdana">Zamów obiad przez internet!</font><h3>';
-$con=mysqli_connect("localhost","michal","michalxsw21qaz","ekosinfa_michal");
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-$result = mysqli_query($con,"SELECT * FROM menu );
-echo "<table border='1'>
-<tr>
-<th>Nazwa</th>
-<th>Opis</th>
-<th>Cena</th>
-</tr>";
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['name'] . "</td>";
-echo "<td>" . $row['cost'] . "</td>";
-echo "<td>" . $row['description'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
-mysqli_close($con);
+
 ?>
