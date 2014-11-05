e3de0d2032876b9593ed3c26dbea37d8da78e99c
michubest <michubest@gmail.com>
2014-11-05 09:15:56 +0100

    Update default.php

--- default.php
+++ default.php
@@ -25,7 +25,7 @@ echo "<tr>";
 echo "<td>" . $row['name'] . "</td>";
 echo "<td>" . $row['description'] . "</td>";
 echo "<td>" . $row['cost'] . " zl </td>";
-echo "<td> <input type='checkbox' name='".$row['name']."' ></td>";
+echo "<td> <input type='checkbox' name='".$row['name']."' value='".$row['name']."'></td>";
 
 echo "</tr>";
 }
