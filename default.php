3c93bf47838b94aa9f47637a8cd7848f8bd5addd
michubest <michubest@gmail.com>
2014-11-06 16:11:07 +0000

    Update default.php

--- default.php
+++ default.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-$result = mysqli_query($con,"SELECT * FROM menu WHERE type ='pizza'");
+$result = mysqli_query($con,"SELECT * FROM pizza");
 echo "<table border='1'>
 <tr>
 <th>nazwa</th>
