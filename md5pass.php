105d8195c36e3f207509cac58d4d4a87aa5450b8
michubest <michubest@gmail.com>
2014-12-02 17:04:08 +0100

    Update md5pass.php

--- md5pass.php
+++ md5pass.php
@@ -1,3 +1,17 @@
-<?php
-$_POST["pass"]=$passw;
+<html>
+<head><title>MD5PASS</title></head>
+<body>
+  <form action="md5pass.php" method="post">
+    <h3>ZASZYFRUJ SWE HASLO</h3><br>
+    <input type="password" name="pass"><br>
+    <input type="submit" value="SZYFRUJ"><br>
+    <?php
+
 echo md5($passw);
+    ?>
+    
+  </form>
+</body>
+  
+</html>
+
