8c9e1007c15014025b02141cd4fb7ddcb8e05f4c
michubest <michubest@gmail.com>
2014-12-02 17:51:24 +0100

    Update sendorder.php

--- pizza/sendorder.php
+++ pizza/sendorder.php
@@ -1,1 +1,12 @@
-<?php ?>
+<?php
+//KONEKSZYNY VARIABELSY I INNE 
+  $con=mysqli_connect("localhost","michal","michalxsw21qaz","ekosinfa_michal");  
+  $query="SELECT * FROM ".$co."";
+  $result = mysqli_query($con,$query);
+
+  while($row = mysqli_fetch_array($result)){
+    if(isset[$row['name']])
+      echo $row['name']."<br>";
+    
+  }
+?>
