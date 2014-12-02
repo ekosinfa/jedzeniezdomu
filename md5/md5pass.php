65771ec863fe180057d810cd3e450989c69ff3c0
michubest <michubest@gmail.com>
2014-12-02 20:54:37 +0100

    Update md5pass.php

--- md5/md5pass.php
+++ md5/md5pass.php
@@ -4,7 +4,7 @@
 <div align="center">
   <form action="md5pass.php" method="post">
     <h3>ZASZYFRUJ SWE HASLO</h3><br>
-    <img src="http://www3.pcmag.com/media/images/370203-hacker-cat.jpg?thumb=y">
+    <img src="http://www3.pcmag.com/media/images/370203-hacker-cat.jpg?thumb=y"><br>
     <input type="password" name="pass" value="<?php echo $_POST['pass'] ?>"><br>
     <input type="submit" value="SZYFRUJ"><br>
     <table><tr><td><?php
