ee1439caaec48be010d4d6d84923a41aee68f960
michubest <michubest@gmail.com>
2014-12-03 10:02:38 +0100

    Update sendorder.php

--- pizza/sendorder.php
+++ pizza/sendorder.php
@@ -10,7 +10,7 @@
       
       echo $row['name']."<br>";
       $cena=$cena + $row['cost'];
-      $zapisz="INSERT INTO orders (order,cena,name) VALUES (".$row['name'].",".$row['cena'].",".'anon')";
+      $zapisz="INSERT INTO orders (order,cena) VALUES (".$row['name'].",".$row['cena'].')'";
       
 }
      
