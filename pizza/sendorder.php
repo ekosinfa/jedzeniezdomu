dd41431dfd776c8b72df3cb7a793ee9ca2d8473f
michubest <michubest@gmail.com>
2014-12-03 22:11:51 +0100

    Update sendorder.php

--- pizza/sendorder.php
+++ pizza/sendorder.php
@@ -6,18 +6,26 @@
   $result = mysqli_query($con,$query);
 echo "Dzieki za zamowienie , zamowiles : <br>";
   while($row = mysqli_fetch_array($result)){
+   $pizza="";
+    if(isset($_POST['cienkieciasto'])){
+      $pizza=$_POST['name']." (cienkie ciasto)";
+   } else{
+      $pizza=$_POST['name']." (grube ciasto)";}
+      
     if(isset($_POST[$row['name']])){
       
       echo "- "  .$row['name']."<br>";
       $cena=$cena + $row['cost'];
       $zapisz="INSERT INTO orders ( name,zamowienie,  cena ) VALUES ('".$_POST['nazwisko']."', '".$row['name']."', '".$row['cost']."' );";
       $query1=mysqli_query($con,$zapisz) or die(mysqli_error($con));
+    
+    }
 }
      
       
     
   }
 
-echo "Przynies ". $cena." zl do sekretariatu aby zrealizować zamówienie";
+echo "Przynies ". $cena." zl do sekretariatu aby zrealizowac zamowienie";
 ?>
 
