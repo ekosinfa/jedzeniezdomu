89ef2ec7eecf76cf71e9f6350cb92b2bc142db3e
michubest <michubest@gmail.com>
2014-12-19 16:59:54 +0100

    Update register.php

--- pizza/register/register.php
+++ pizza/register/register.php
@@ -1,1 +1,36 @@
-<?php ?>
+<?php 
+//Sprawdzanie,czy zmienne są ustawione i inne rzeczy
+if(empty($_POST['username'])){
+  exit('ERROR:Nie wpisales nazwy uzytkownika');
+}
+if(empty($_POST['email'])){
+  exit('ERROR:Nie wpisales adresu email');
+}
+if(empty($_POST['imienazwisko'])){
+  exit('ERROR:Nie wpisales swego imienia i nazwiska');
+}
+if(empty($_POST['haslo1'])){
+  exit('ERROR:Nie wpisales hasla');
+}
+if(empty($_POST['haslo2'])){
+  exit('ERROR:Nie wpisales hasla');
+}
+if($_POST['haslo1']!=$_POST['haslo2']){
+  exit('ERROR:Nie wpisales username');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
