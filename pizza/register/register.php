5ce0fe250bae0189069891eb36f7729895833340
michubest <michubest@gmail.com>
2014-12-19 17:21:56 +0100

    Update register.php

--- pizza/register/register.php
+++ pizza/register/register.php
@@ -18,6 +18,10 @@ if(empty($_POST['haslo2'])){
 if($_POST['haslo1']!=$_POST['haslo2']){
   exit('ERROR:hasla nie sa identyczne');
 }
+if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL );==false){
+  exit('adres e-mail nie jest prawidlowy')
+}
+  
 
 
 
