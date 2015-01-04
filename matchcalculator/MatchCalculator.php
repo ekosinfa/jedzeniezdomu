bba04f377dc8641e31779bb03b8212510a49d215
michubest <michubest@gmail.com>
2015-01-04 17:16:26 +0100

    Update MatchCalculator.php

--- matchcalculator/MatchCalculator.php
+++ matchcalculator/MatchCalculator.php
@@ -1,5 +1,6 @@
 <?php
 $t1=$_POST['t1p1'] + $_POST['t1p2'] + $_POST['t1p3'] + $_POST['t1p4'] + $_POST['t1p5'];
 $t2=$_POST['t2p1'] + $_POST['t2p2'] + $_POST['t2p3'] + $_POST['t2p4'] + $_POST['t2p5'];
-
+$t1=$t1 /5;
+$t2=$t2 /5;
 ?>
