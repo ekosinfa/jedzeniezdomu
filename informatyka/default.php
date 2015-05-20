d3e9c94e9012021984c721cabd6e0662912e2c8c
michubest <michubest@gmail.com>
2015-05-20 04:57:19 -0400

    A

--- informatyka/default.php
+++ informatyka/default.php
@@ -24,12 +24,9 @@
 						echo "<br />";
 						echo "<center><h4>Smieszny obrazek nr ".$row['id']."</h4><br />";
 						echo '<img src="'.$row['link'].'"<br />';
-						echo '<h5><a href="'.$row['zrodlo'].'">zrodlo </h5></center>';
+						echo '<h5><a href="'.$row['zrodlo'].'">zrodlo </h5></center></a>';
 						}
 ?>
-			test
-		</body>
-</html>
-			test
+			
 		</body>
 </html>
