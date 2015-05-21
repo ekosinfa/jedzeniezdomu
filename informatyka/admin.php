99c3245d584fde35de2b63401b20c77980e17c4e
michubest <michubest@gmail.com>
2015-05-21 12:30:19 -0400

    removed bugs

--- informatyka/admin.php
+++ informatyka/admin.php
@@ -4,14 +4,16 @@
 				ini_set('display_startup_errors',1);
 				error_reporting(-1);
 				
+				
 				$con=mysqli_connect("localhost","michal","michalxsw21qaz","ekosinfa_michal");  
-				$result = mysqli_query($con,$query);
+				
 	//Sprawdzenie hasla
 	if($_POST['password'] != "imbir"){
 		exit('Zle haslo , kliknij <a href="admin.html">tu</a> aby powrócić do panelu admina.');
 		}
-			$query = "INSERT INTO obrazki (link,zrodlo) VALUES (".$_POST['link'].",".$_POST['zrodlo'].")"
-
+			$query = "INSERT INTO obrazki (link,zrodlo) VALUES (".$_POST['link'].",".$_POST['zrodlo'].")";
+			$result = mysqli_query($con,$query);
+if(!result){exit("problem z baza danych mysql");}
 
 
 
