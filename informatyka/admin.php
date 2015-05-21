e7f933834ae80df59f329e3dff583fbaabeb65eb
michubest <michubest@gmail.com>
2015-05-21 12:34:37 -0400

    fixed more bugs

--- informatyka/admin.php
+++ informatyka/admin.php
@@ -3,13 +3,13 @@
 				ini_set('display_errors',1);
 				ini_set('display_startup_errors',1);
 				error_reporting(-1);
-				
+				echo"<meta charset="utf-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome = 1"";
 				
 				$con=mysqli_connect("localhost","michal","michalxsw21qaz","ekosinfa_michal");  
 				
 	//Sprawdzenie hasla
 	if($_POST['password'] != "imbir"){
-		exit('Zle haslo , kliknij <a href="admin.html">tu</a> aby powrócić do panelu admina.');
+		exit('Zle haslo , kliknij <a href="admin.html">tu</a> aby powroicdo panelu admina.');
 		}
 			$query = "INSERT INTO obrazki (link,zrodlo) VALUES (".$_POST['link'].",".$_POST['zrodlo'].")";
 			$result = mysqli_query($con,$query);
