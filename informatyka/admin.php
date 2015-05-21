f36da23c54debef3a4d969fc2a8a1b7dca2337eb
michubest <michubest@gmail.com>
2015-05-21 12:11:20 -0400

    a

--- informatyka/admin.php
+++ informatyka/admin.php
@@ -1,6 +1,14 @@
 <?php
-
-
+				ini_set('display_errors',1);
+				ini_set('display_startup_errors',1);
+				error_reporting(-1);
+				
+				$con=mysqli_connect("localhost","michal","michalxsw21qaz","ekosinfa_michal");  
+				$query="SELECT * FROM obrazki ORDER BY rank";
+				$result = mysqli_query($con,$query);
+if($_POST['password'] != "imbir"){
+	exit('Zle haslo , kliknij <a href="admin.html">tu</a> aby powrócić do panelu admina.')
+	}
 
 
 
